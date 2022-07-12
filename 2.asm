16aac0de920d3a62ef428b917371a4847e5f0ac9
Vadim Čeremisinov <94128424+FasterThanC@users.noreply.github.com>
2022-07-12 22:33:12 +0300

    Add files via upload

--- 2.asm
+++ 2.asm
@@ -1,8 +1,8 @@
-;Vadim ÄŒeremisinov 4 grupÄ—
+;Vadim Èeremisinov 4 grupë
 ;LSP numeris: 2110545
-;ParaÅ¡ykite programÄ…, kuri atlieka operacijÄ… XOR 
-;dviems beveik bet kokio ilgio dvejetainiams skaiÄiams, esantiems failuose, 
-;ir iÅ¡veda rezultatÄ… Ä¯ treÄiÄ… failÄ….
+;Parağykite programà, kuri atlieka operacijà XOR 
+;dviems beveik bet kokio ilgio dvejetainiams skaièiams, esantiems failuose, 
+;ir iğveda rezultatà á treèià failà.
 
 
 .model small		    
